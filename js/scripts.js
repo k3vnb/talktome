e651fa502c5723433f2b3b1ac374bf57eeee50e6
John Sawiris and Kevin Boyle <kevin@>
2017-10-17 15:26:08 -0700

    add img format img

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,33 @@ $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#dog").prepend("<li>Woof!</li>");
     $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#purrr").click(function() {
     $("ul#dog").prepend("<li>Grrr!</li>");
     $("ul#cat").prepend("<li>Prrr!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#dog").prepend("<li>Hey, stop screaming!</li>");
     $("ul#cat").prepend("<li>Screetch!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
